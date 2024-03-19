1f8b62484c7b06632f0c96eb76e9dca0fc77dec9
Sai Pradeep Vamala <sai.vamala@wenable.com>
2024-03-19 22:41:43 +0530

    Logic for fall

--- app/src/main/java/com/weguard/gyroscopesensor/MainActivity.kt
+++ app/src/main/java/com/weguard/gyroscopesensor/MainActivity.kt
@@ -16,10 +16,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.weguard.gyroscopesensor.ui.theme.GyroscopeSensorTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.math.abs
 import kotlin.math.sqrt
 
 class MainActivity : ComponentActivity(), SensorEventListener {
@@ -29,6 +36,7 @@ class MainActivity : ComponentActivity(), SensorEventListener {
     private val linearAcceleration = FloatArray(3)
     private val gravity = FloatArray(3)
     private var callStatus: Boolean = false
+    private var xArray = ArrayList<Float>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -82,26 +90,18 @@ class MainActivity : ComponentActivity(), SensorEventListener {
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
-        // This time stamp delta rotation to be multiplied by the current rotation
-        // after computing it from the gyro sample data.
         if (event != null) {
 
             when (event.sensor) {
 
                 gyroscope -> {
-                    // Axis of the rotation sample, not normalized yet.
                     var axisX: Float = event.values[0]
-                    Log.d("Axis-X", "$axisX")
                     var axisY: Float = event.values[1]
-                    Log.d("Axis-Y", "$axisY")
                     var axisZ: Float = event.values[2]
-                    Log.d("Axis-Z", "$axisZ")
 
-//                 Calculate the angular speed of the sample
 //                 Angular velocity magnitude refers to the overall rate of rotation of an object around an axis,
 //                 regardless of the direction of rotation
                     val omegaMagnitude: Float = sqrt(axisX * axisX + axisY * axisY + axisZ * axisZ)
-                    Log.d("omegaMagnitude", "$omegaMagnitude")
 
                     // Normalize the rotation vector if it's big enough to get the axis
                     // (i.e., EPSILON should represent your maximum allowable margin of error)
@@ -120,32 +120,51 @@ class MainActivity : ComponentActivity(), SensorEventListener {
                 }
 
                 accelerometer -> {
-//                  alpha is calculated as t / (t + dT)
-                    val alpha = 0.8f
+                    val alpha = 0.8f //  alpha is calculated as t / (t + dT)
                     val fallThreshold = -2f
 
                     // Isolate the force of gravity with the low-pass filter.
                     gravity[0] = alpha * gravity[0] + ((1 - alpha) * event.values[0])
-                    Log.d("gravity X-Axis", "${gravity[0]}")
                     gravity[1] = alpha * gravity[1] + ((1 - alpha) * event.values[1])
-                    Log.d("gravity Y-Axis", "${gravity[1]}")
                     gravity[2] = alpha * gravity[2] + ((1 - alpha) * event.values[2])
-                    Log.d("gravity Z-Axis", "${gravity[2]}")
 
                     // Remove the gravity contribution with the high-pass filter.
                     linearAcceleration[0] = event.values[0] - gravity[0]
-                    Log.d("Acceleration X-Axis", "${linearAcceleration[0]}")
                     linearAcceleration[1] = event.values[1] - gravity[1]
-                    Log.d("Acceleration Y-Axis", "${linearAcceleration[1]}")
                     linearAcceleration[2] = event.values[2] - gravity[2]
-                    Log.d("Acceleration Z-Axis", "${linearAcceleration[2]}")
 
+                    // Gravity check needs to be added.
                     if (linearAcceleration[1] < fallThreshold) {
-                        Log.d("linearAcceleration", "${linearAcceleration[2]}")
-                        Utils.setAccelerometerDetection(1)
-                        callStatus = true
+                        val x = abs(linearAcceleration[1])
+                        xArray.add(x)
+                        Log.d("nowX", "x: $x, xArray: $xArray and Size: ${xArray.size}")
+                        if (xArray.size > 1) {
+                            for (i in 0 until xArray.size - 1) {
+                                val iNow = xArray[i]
+                                val iOnIncrement = xArray[i + 1]
+                                Log.d("nowX", "i: $iNow, i+1: $iOnIncrement")
+                                if (iNow < iOnIncrement) {
+                                    // Fall need to have an sudden increase.
+                                    CoroutineScope(Dispatchers.IO).launch {
+                                        delay(800)
+                                        val y: Float = abs(linearAcceleration[1])
+                                        Log.d("linearAcceleration", "Y: $y") // ~ 0f
+                                        val z = x - y
+                                        val r = x / 2
+                                        callStatus = if (z > r && y > 0.5) {
+                                            Log.d("FallState", "Fall x: $x, y: $y, z: $z, r: $r")
+                                            Utils.setAccelerometerDetection(1)
+                                            true
+                                        } else {
+                                            Log.d("FallState", "No Fall x: $x, y: $y, z: $z, r: $r")
+                                            Utils.setAccelerometerDetection(0)
+                                            false
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     } else {
-                        Log.d("linearAcceleration", "Safe....!")
                         Utils.setAccelerometerDetection(0)
                     }
                 }
@@ -167,7 +186,16 @@ fun AppNavigations() {
         composable(route = "HomePage") {
             EmergencyCall(navController)
         }
-        composable(route = "DetectionPage") { IfDetectionHappened(navController) }
+        composable(
+            route = "DetectionPage/{toStartIt}",
+            arguments = listOf(navArgument("toStartIt") { type = NavType.BoolType })
+        ) { navBackstackEntry ->
+            val startAction = navBackstackEntry.arguments
+                ?.getBoolean("toStartIt")
+            startAction?.let { actionValue ->
+                IfDetectionHappened(navController,actionValue)
+            }
+        }
     }
 }
 
